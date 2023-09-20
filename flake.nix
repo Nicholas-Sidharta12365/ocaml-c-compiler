2e65c3fc02ff36265a5b2a81edc1babc5cbc4a9c
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-09-20 16:02:22 +0700

    initialize repository

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "OCaml development environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs =
+    { self
+    , flake-utils
+    , nixpkgs
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ ocaml ocamlformat ] ++
+          (with pkgs.ocamlPackages; [ dune_3 odoc ]);
+
+        shellHook = ''
+          ${pkgs.ocaml}/bin/ocaml --version
+        '';
+      };
+    });
+}
