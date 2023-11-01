ee9f512fd311e06568a61c7c460cffa43a221321
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-11-01 11:05:14 +0700

    chore: downgrade dune 3.6

--- flake.nix
+++ flake.nix
@@ -17,6 +17,14 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
+      packages = {
+        # nix run .#dev 
+        dev = pkgs.writeShellScriptBin "dev" ''
+          cd "$(git rev-parse --show-toplevel)"
+          dune exec ocaml_c_compiler
+        '';
+      };
+
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [ ocaml ocamlformat ] ++
           (with pkgs.ocamlPackages; [ dune_3 odoc ]);
